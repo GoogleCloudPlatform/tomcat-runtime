ce8333559538a4fc4f722194292665d02a08529b
Damien Cassan <damiencassan@google.com>
2017-06-09 16:52:14 -0400

    Add separate release script

---
+++ scripts/ci/release.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Copyright 2017 Google Inc. All rights reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+dir=$(dirname $0)
+projectRoot=${dir}/../..
+RUNTIME_NAME='tomcat'
+RUNTIME_VERSION='8.5'
+
+if [ -z "${DOCKER_TAG}" ]; then
+  DOCKER_TAG="${RUNTIME_VERSION}-$(date -u +%Y-%m-%d_%H_%M)"
+fi
+
+if [ -z "$DOCKER_NAMESPACE" ]; then
+  DOCKER_NAMESPACE="gcr.io/$(gcloud info \
+                | awk '/^Project: / { print $2 }' \
+                | sed 's/\[//'  \
+                | sed 's/\]//')"
+fi
+
+IMAGE="${DOCKER_NAMESPACE}/${RUNTIME_NAME}:${DOCKER_TAG}"
+
+gcloud container builds submit \
+        --config ${dir}/release-cloudbuild.yaml \
+        --substitutions="_IMAGE=$IMAGE,_DOCKER_TAG=$DOCKER_TAG" \
+        ${projectRoot}
