7774d5b3a3eef5e773fea29f684c94f9d79e12bd
Damien Cassan <damiencassan@google.com>
2017-06-16 16:30:32 -0400

    Add license to new Java files

--- tests/test-war-integration/src/main/java/com/google/cloud/runtimes/tomcat/test/simple/SecureServlet.java
+++ tests/test-war-integration/src/main/java/com/google/cloud/runtimes/tomcat/test/simple/SecureServlet.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.cloud.runtimes.tomcat.test.simple;
 
 import java.io.IOException;
@@ -9,7 +25,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * This class is use by the the runtime common testing framework as a custom test to ensure
+ * This class is used by the the runtime common testing framework as a custom test to ensure
  * that when x-forwarded-proto is specified and equals to https
  * (e.g the application is served by a load balancer)
  * the connection is considered as secure.
