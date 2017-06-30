a08e89f9d04b73f412cea6d3b67b29fa2d219b61
Damien Cassan <damiencassan@google.com>
2017-06-30 17:06:46 -0400

    Enable distributed session based on environment variable

--- tomcat/src/main/docker/50-tomcat.bash
+++ tomcat/src/main/docker/50-tomcat.bash
@@ -4,3 +4,7 @@
 if [ "$PLATFORM" == "gae" ]; then
   cp /config/gcp.xml ${CATALINA_BASE}/conf/gcp.xml
 fi
+
+if [ "$ENABLE_DISTRIBUTED_SESSION" == "true" ]; then
+  cp /config/distributed-session.xml ${CATALINA_BASE}/conf/distributed-session.xml
+fi
