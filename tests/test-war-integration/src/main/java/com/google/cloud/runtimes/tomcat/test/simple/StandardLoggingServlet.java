b4ab7138018ebb3a8d8ca8f9cdbc3ce30c01f2f1
Damien Cassan <damiencassan@google.com>
2017-08-09 10:37:11 -0400

    Refactor SandardLoggingServlet

--- tests/test-war-integration/src/main/java/com/google/cloud/runtimes/tomcat/test/simple/StandardLoggingServlet.java
+++ tests/test-war-integration/src/main/java/com/google/cloud/runtimes/tomcat/test/simple/StandardLoggingServlet.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.cloud.runtimes.tomcat.test.simple;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -17,26 +32,32 @@ public class StandardLoggingServlet extends HttpServlet {
 
   private static final Logger logger = Logger.getLogger(StandardLoggingServlet.class.getName());
 
+  private static final ObjectMapper objectMapper = new ObjectMapper();
+
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-
-    ObjectMapper objectMapper = new ObjectMapper();
     JsonNode body = objectMapper.readTree(req.getReader());
     String token = body.path("token").asText();
-    String level = body.path("level").asText();
-
-    if (level.equals("DEBUG")) {
-      level = "FINE";
-    } else if ( level.equals("ERROR") ||
-                level.equals("CRITICAL") ||
-                level.equals("ALERT")) {
-      level = "SEVERE";
-    }
-
+    String level = convertStackdriverSeverityToLoggingLevel(body.path("level").asText());
 
     logger.log(Level.parse(level), token);
+
     resp.setContentType("text/plain");
     resp.getWriter().println(URLEncoder.encode("appengine.googleapis.com/stdout", "UTF-8"));
+  }
+
+  private String convertStackdriverSeverityToLoggingLevel(String level) {
+    switch (level) {
+      case "DEBUG":
+        level = "FINE";
+        break;
+      case "ERROR":
+      case "CRITICAL":
+      case "ALERT":
+        level = "SEVERE";
+    }
+
+    return level;
   }
 }
