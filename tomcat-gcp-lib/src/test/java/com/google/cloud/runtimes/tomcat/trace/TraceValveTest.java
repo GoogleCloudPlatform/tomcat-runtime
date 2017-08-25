455e1a0e8b9593df47f57ae5223dfdb63e544467
Damien Cassan <damiencassan@google.com>
2017-08-25 13:56:43 -0400

    Refactor the logging package to trace

--- tomcat-gcp-lib/src/test/java/com/google/cloud/runtimes/tomcat/trace/TraceValveTest.java
+++ tomcat-gcp-lib/src/test/java/com/google/cloud/runtimes/tomcat/trace/TraceValveTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.cloud.runtimes.tomcat.logging;
+package com.google.cloud.runtimes.tomcat.trace;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
